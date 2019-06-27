8e526fa17c2b927a06aad2cd6b69393a0970bd86
BRUNO ZAMBOTE RIBEIRO <171151797@acad.dvuadmin.net>
2019-06-26 21:06:12 -0300

    projeto para corrigir

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
- 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://admin:ch3ckd1sk@capacitacao-api-facul-00dsw.mongodb.net/test?retryWrites=true&w=majority',{
+    useNewUrlParser: true
+});
+
 app.use('/', require('./routes'));
 
 module.exports = app;
